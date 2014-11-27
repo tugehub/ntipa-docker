f93b36520bc5526d6be5d9af0267f9dacb731d2d
tindaro tornabene <tindaro.tornabene@gmail.com>
2014-11-27 17:58:52 +0100

    add

--- ntipa-postgres/start.sh
+++ ntipa-postgres/start.sh
@@ -5,25 +5,30 @@ CONF="/etc/postgresql/9.3/main/postgresql.conf"
 POSTGRES="/usr/lib/postgresql/9.3/bin/postgres"
 INITDB="/usr/lib/postgresql/9.3/bin/initdb"
 
-# test if DATADIR is existent
 if [ ! -d $DATADIR ]; then
   echo "Creating Postgres data at $DATADIR"
   mkdir -p $DATADIR
 fi
 
-# test if DATADIR has content
 if [ ! "$(ls -A $DATADIR)" ]; then
   echo "Initializing Postgres Database at $DATADIR"
   chown -R postgres $DATADIR
   su postgres sh -c "$INITDB $DATADIR"
   su postgres sh -c "$POSTGRES --single -D $DATADIR -c config_file=$CONF" <<< "CREATE USER ntipa WITH SUPERUSER PASSWORD 'ntipa';"
+ 
 fi
+service postgresql start
+echo "Creating Postgres DB for NTIPA"
+su - postgres  -c  "createdb -O ntipa ntipa-manager" 
+su - postgres  -c  "createdb -O ntipa ntipa-protocollo"
+su - postgres  -c  "createdb -O ntipa ntipa-manager-dev"
+su - postgres  -c  "createdb -O ntipa ntipa-protocollo-dev"
+su - postgres  -c  "createdb -O ntipa ntipa-jbilling"
+su - postgres  -c  "createdb -O ntipa ntipa-jbilling-dev" 
+su - postgres  -c  "createdb -O ntipa ntipa-drupal" 
+su - postgres  -c  "createdb -O ntipa ntipa-drupal-dev"
+service postgresql stop
 
 trap "echo \"Sending SIGTERM to postgres\"; killall -s SIGTERM postgres" SIGTERM
-
-su postgres sh -c "$POSTGRES -D $DATADIR -c config_file=$CONF" &
-
-
-
-
+su postgres sh -c "$POSTGRES -D $DATADIR -c config_file=$CONF"
 wait $!
